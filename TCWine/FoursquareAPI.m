3876ce338e1921b0d13ac9e4b7c1aa51b9f56981
Donovan Cotter <donovan.cotter@yahoo.com>
2016-03-09 11:20:06 -0500

    removed mapView param from Foursquare Winery API call

--- TCWine/FoursquareAPI.m
+++ TCWine/FoursquareAPI.m
@@ -30,7 +30,7 @@
     return [[self alloc]initWithClientSecret:clientSecret clientID:clientID categoryId:categoryId];
     }
 
--(void)foursquareAPI:(NSMutableArray *)wineryArray mapView:(MKMapView *)mapView handler:(void(^)(NSDictionary *data))handler {
+-(void)foursquareAPI:(NSMutableArray *)wineryArray handler:(void(^)(NSDictionary *data))handler {
     
     NSURL *url = [NSURL URLWithString:_foursquareAPIURLString];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
@@ -49,59 +49,6 @@
     [operation start];
 
 }
-
-//-(void)foursquareAPI:(NSMutableArray *)wineryArray mapView:(MKMapView *)mapView {
-//    NSURL *url = [NSURL URLWithString:_foursquareAPIURLString];
-//    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
-//    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
-//    
-//    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-//        
-//        if (!error) {
-//            NSHTTPURLResponse *URLResponse = (NSHTTPURLResponse*) response;
-//            
-//            if (URLResponse.statusCode == 200) {
-//                NSError *JSONError;
-//                
-//                NSDictionary *foursquareJSON = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&JSONError];
-//                
-//                if (!JSONError) {
-//
-//                    NSMutableArray *jsonResponse = [foursquareJSON valueForKeyPath: @"response.venues"];
-//                    
-//                    for (NSDictionary *foursquareData in jsonResponse) {
-//        
-//                        Winery *winery = [Winery initWithWineryName:[foursquareData valueForKey:@"name"]];
-//                        winery.address = [foursquareData valueForKeyPath:@"location.formattedAddress"];
-//                        winery.phoneNumber = [foursquareData valueForKeyPath:@"contact.formattedPhone"];
-//                        winery.website = [foursquareData valueForKeyPath:@"url"];
-//                        winery.longitude = [[foursquareData valueForKeyPath:@"location.lng"] doubleValue];
-//                        winery.latitude = [[foursquareData valueForKeyPath:@"location.lat"] doubleValue];
-//                        winery.wineryId = [foursquareData valueForKeyPath:@"id"];
-//                        NSMutableArray *formattedAddress = [foursquareData valueForKeyPath:@"location.formattedAddress"];
-//                        NSString *fullAddress = [NSString stringWithFormat: @"%@, %@", formattedAddress[0], formattedAddress[1]];
-//                        winery.address = fullAddress;
-//                        
-//                        [wineryArray addObject:winery];
-//                    
-//                    }
-//
-//                    dispatch_async(dispatch_get_main_queue(), ^{
-//                        [self createAnnotation:mapView wineryArray:wineryArray];
-//                    });
-//                    
-//                } else {
-//                    NSLog(@"ERROR with JSON");
-//                }
-//            }
-//        } else {
-//            NSLog(@"ERROR with Network Call");
-//        }
-//        
-//    }];
-//
-//    [dataTask resume];
-//}
 
 -(void)createAnnotation:(MKMapView *)mapView wineryArray:(NSMutableArray *)wineryArray {
     for (Winery *winery in wineryArray) {
